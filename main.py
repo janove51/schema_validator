297ff01775ed1a7b05763a138a49906f953968be
janove51 <Dfli309##>
2021-05-08 11:02:05 +0200

    add pydantic model

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import ndjson
-
+import pydantic_models.models
 
 data_input_file = "./input_data/input.json"
+
 
 with open(data_input_file) as file:
     ndjson_reader = ndjson.reader(file)
     for row in ndjson_reader:
-
         print(row)
-
+        pydantic_models.models.EventStreamX.parse_obj(row)
+# TODO: add timestamp validation
